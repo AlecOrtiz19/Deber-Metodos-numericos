90d086d84494496e37dc485ebb6d471d3f530d0a
AlecOrtiz19 <137963274+AlecOrtiz19@users.noreply.github.com>
2024-05-08 00:19:27 -0500

    Update Metodos4.py

--- Metodos4.py
+++ Metodos4.py
@@ -1,11 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Datos conocidos (puntos x, y)
-x_datos = np.array([2, 3, 4, 5, 6])  # Suponiendo que tienes puntos en x
-y_datos = np.array([1, 3, 8, 5])  # Suponiendo que tienes puntos en y
+x_datos = np.array([2, 3, 4, 5, 6])  
+y_datos = np.array([1, 3, 8, 5])  
 
-# Función para calcular el polinomio de interpolación de Lagrange de tercer grado
+"""
+Utilice un código de programación de Python para realizar los ejercicios b, c, d, e. 
+Como resultado coloque el código, si no se ve bien, se puede colocar como imagen. Además, 
+
+Ejercicio E
+
+"""
+
 def lagrange_tercer_grado(x, x_datos, y_datos):
     n = len(x_datos)
     resultado = 0
@@ -19,12 +25,10 @@ def lagrange_tercer_grado(x, x_datos, y_datos):
         resultado += termino
     return resultado
 
-# Estimación para x = 3.3
 x_estimado = 3.3
 y_estimado = lagrange_tercer_grado(x_estimado, x_datos, y_datos)
 print("Estimación para x = 3.3:", y_estimado)
 
-# Graficar el polinomio de interpolación de Lagrange y los puntos de datos
 x_valores = np.linspace(min(x_datos), max(x_datos), 100)
 y_valores = lagrange_tercer_grado(x_valores, x_datos, y_datos)
 
