b36cc67b4cdf252476cf43574373b44260d1724b
AlecOrtiz19 <137963274+AlecOrtiz19@users.noreply.github.com>
2024-05-08 23:05:36 -0500

    Update Metodos2.py

--- Metodos2.py
+++ Metodos2.py
@@ -1,9 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-x_datos = np.array([3, 4, 5])  
-y_datos = np.array([1, 3, 8])  
+x_datos = np.array([2,3, 4, 5])  
+y_datos = np.array([1.20, 1.90, 2.40, 2.79])  
 
 """
 Utilice un código de programación de Python para realizar los ejercicios b, c, d, e. 
@@ -12,6 +11,7 @@ Como resultado coloque el código, si no se ve bien, se puede colocar como image
 Ejercicio c
 
 """
+
 def lagrange_segundo_grado(x, x_datos, y_datos):
     n = len(x_datos)
     resultado = 0
@@ -27,11 +27,9 @@ x_estimado = 3.3
 y_estimado = lagrange_segundo_grado(x_estimado, x_datos, y_datos)
 print("Estimación para x = 3.3:", y_estimado)
 
-
 valor_real = 2.05  
 error_relativo = abs((y_estimado - valor_real) / valor_real) * 100
 print("Error relativo:", error_relativo, "%")
-
 
 x_valores = np.linspace(min(x_datos), max(x_datos), 100)
 y_valores = lagrange_segundo_grado(x_valores, x_datos, y_datos)
